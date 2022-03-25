613380435fdb8bee5ead0e21ff8ee943198cb5e7
cshm <cshm225@gmail.com>
2022-03-25 11:00:04 +0900

    Matsuoka作成

--- Matsuoka.java
+++ Matsuoka.java
@@ -3,13 +3,10 @@ public class Matsuoka{
 	public static void main(String[] args){
 		long dis=384400000000l,paper=1l;
 		int count=0;
-		while(paper<384400000000l){
+		while(paper<dis){
 			count++;
 			paper*=2;
-			System.out.println(count+"回折り曲げた("+paper+")");
-			if(paper>=dis){
-				break;
-			}
+			System.out.println(count+"回折り曲げた("+paper+"mm)");
 		}
 		System.out.println("折った回数は"+count+"回");
 	}
